6b1be9fb941bd8efffd7de6e9806c9cccc536f10
TomasMacoun <macount@seznam.cz>
2023-05-03 12:27:23 +0200

    Completed Tests for MatrixAddition Task

--- src/additionaltasks/twodarrays/MatrixAddition.java
+++ src/additionaltasks/twodarrays/MatrixAddition.java
@@ -1,2 +1,23 @@
-package additionaltasks.twodarrays;public class MatrixAddition {
+package additionaltasks.twodarrays;
+public class MatrixAddition {
+
+    public static int[][] matrixAddition (int[][] matrix1, int[][] matrix2) throws Exception {
+        int rows1 = matrix1.length;
+        int cols1 = matrix1[0].length;
+        int rows2 = matrix2.length;
+        int cols2 = matrix2[0].length;
+        if (rows1 != rows2 || cols1 != cols2) {
+            throw new Exception("Invalid input");
+        }
+        int[][] result = new int[rows1][cols1];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols1; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        return result;
+    }
 }
+
+
+
--- src/additionaltasks/twodarrays/MatrixAdditionTest.java
+++ src/additionaltasks/twodarrays/MatrixAdditionTest.java
@@ -1,4 +1,39 @@
+package additionaltasks.twodarrays;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MatrixAdditionTest {
-  
+
+    @Test
+    @DisplayName("Test for same number of rows and columns.")
+    void matrixAddition() throws Exception {
+        int[][] matrix1 = {{1, 0, 1}, {0, 2, 2}, {1, 2, 5}};
+        int[][] matrix2 = {{3, 0, 1}, {0, 2, 2}, {1, 2, 5}};
+
+        int [][] expected = {{4, 0, 2}, {0, 4, 4}, {2, 4, 10}};
+
+        int[][] result = MatrixAddition.matrixAddition(matrix1, matrix2);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testMatrixAdditionInvalidInput() {
+        int[][] matrix1 = {{1, 2}, {3, 4}};
+        int[][] matrix2 = {{5, 6}, {7, 8}, {9, 10}};
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            MatrixAddition.matrixAddition(matrix1, matrix2);
+        });
+
+        Assertions.assertEquals("Invalid input", thrown.getMessage());
+    }
 }
